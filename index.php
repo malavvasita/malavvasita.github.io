16246ce224b0c73c6b3272ea31272bdd382299ca
Malav Vasita <malav.vasita@rtcamp.com>
2018-07-30 01:04:26 +0530

    Convert site from PHP to HTML

--- index.php
+++ index.php
@@ -10,33 +10,297 @@
 
     <title>Malav Vasita | Web Engineer</title>
 
-    <?php include_once './theme-parts/header-scripts.php'; ?>
+        <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/png" href="./img/favicon.png"/>
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Merriweather+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="vendor/devicons/css/devicons.min.css" rel="stylesheet">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/resume.min.css" rel="stylesheet">
 
   </head>
 
   <body id="page-top">
 
-    <?php include_once './theme-parts/nav.php'; ?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#experience">Experience</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#education">Education</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#skills">Skills</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#interests">Interests</a>
+          </li>
+        </ul>
+      </div>
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none">Start Bootstrap</span>
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile mx-auto mb-2" src="img/profile.jpeg" alt="">
+        </span>
+      </a>
+      <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" target="_blank" href="https://docs.google.com/document/d/13-IjK6tHVxnhkChL3tlAl5SikQ4nCVWmGEjemodrxNU/export?format=pdf">Download Resume</a>
+          </li>
+        </ul>
+    </nav>
 
     <div class="container-fluid p-0">
 
-      <?php
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+        <div class="my-auto">
+          <h1 class="mb-0">Malav
+            <span class="text-primary">Vasita</span>
+          </h1>
+          <hr>
+          <div class="subheading mb-5">Web Engineer ·
+            <a href="mailto:right.malav@outlook.com">right.malav@outlook.com</a>
+          </div>
+          <p class="mb-5">I have completed my masters from Nirma University. I have been developing as a front end as well as a back end developer from the past <?php echo date( 'Y' ) - date( '2015'); ?> years. I’ve been working with rtCamp in Pune from January, 2018. I am always motivated to take up challenges. When free, I like listening to music, sing and play cricket.</p>
+          <ul class="list-inline list-social-icons mb-0">
+            <li class="list-inline-item">
+              <a href="https://www.facebook.com/malav.vasita.am" target="_blank">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://twitter.com/malavvasita" target="_blank">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.linkedin.com/in/malav-vasita-39843190/" target="_blank">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://github.com/malavvasita" target="_blank">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
 
-      include_once './theme-parts/home.php';
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
+        <div class="my-auto">
+          <h2 class="mb-5">Experience</h2>
 
-      include_once './theme-parts/experience.php';
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Web Engineer</h3>
+              <div class="subheading mb-3">rtCamp Solutions Private Limited</div>
+              <ul class="list-inline list-social-icons mb-0">
+                <li class="list-inline-item" title="rtMedia">
+                <a href="https://github.com/malavvasita/rtMedia" target="_blank">
+                  <span class="fa-stack fa-lg">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-link fa-stack-1x fa-inverse"></i>
+                  </span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">January 2018 - Present</span>
+            </div>
+          </div>
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">PHP Developer</h3>
+              <div class="subheading mb-3">Right InfoService</div>
+              <ul class="list-inline list-social-icons mb-0">
+                <li class="list-inline-item" title="Right InfoService">
+                <a href="http://rightinfoservice.com/" target="_blank">
+                  <span class="fa-stack fa-lg">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-link fa-stack-1x fa-inverse"></i>
+                  </span>
+                  </a>
+                </li>
+                <li class="list-inline-item" title="DJ Abhi - Ahmedabad">
+                <a href="http://djabhi.co.in/" target="_blank">
+                  <span class="fa-stack fa-lg">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-link fa-stack-1x fa-inverse"></i>
+                  </span>
+                  </a>
+                </li>
+                <li class="list-inline-item" title="SBPT Marriage Consultant">
+                <a href="http://sbptjeevansathi.com/" target="_blank">
+                  <span class="fa-stack fa-lg">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-link fa-stack-1x fa-inverse"></i>
+                  </span>
+                  </a>
+                </li>
+                <li class="list-inline-item" title="Gambh Gole Community">
+                <a href="http://gamphgole.org/" target="_blank">
+                  <span class="fa-stack fa-lg">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-link fa-stack-1x fa-inverse"></i>
+                  </span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">January 2015 - June 2017</span>
+            </div>
+          </div>
+        </div>
 
-      include_once './theme-parts/education.php';
+      </section>
 
-      include_once './theme-parts/skills.php';
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="education">
+        <div class="my-auto">
+          <h2 class="mb-5">Education</h2>
 
-      include_once './theme-parts/interests.php';
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Institute of Technology, Nirma University</h3>
+              <div class="subheading mb-3">Masters of Computer Application</div>
+              <p>PPI: 8.19</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">August 2015 - April 2018</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">GLS Institute of Comuter Application</h3>
+              <div class="subheading mb-3">Bachelors of Computer Application</div>
+              <p>PPI: 8.13</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">July 2012 - April 2015</span>
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
+        <div class="my-auto">
+          <h2 class="mb-5">Skills</h2>
+
+          <div class="subheading mb-3">Programming Languages &amp; Tools</div>
+          <ul class="list-inline list-icons">
+            <li class="list-inline-item">
+              <i class="devicons devicons-wordpress"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-html5"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-css3"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-javascript"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-jquery"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-bootstrap"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-sass"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-grunt"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-gulp"></i>
+            </li>
+          </ul>
+
+          <div class="subheading mb-3">More,</div>
+          <ul class="fa-ul mb-0">
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Responsive Design, Mobile First</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Dynammic Websites</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Custom WordPress themes &amp; plugin development</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              On time delivery</li>
+          </ul>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
+        <div class="my-auto">
+          <h2 class="mb-5">Interests</h2>
+          <div class="subheading mb-3">Apart from development, I like to</div>
+          <ul class="fa-ul mb-0">
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Do tracking</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Do Singing</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Play Cricket</li>
+          </ul><br><hr/>
+          <p style="text-align: center;"><b>I believe in Less but <span style="color:#000">Beautiful Code</span>. &#x1f609;</b></p>
+          <hr/>
+        </div>
+        </div>
+      </section>
 
       ?>
 
     </div>
 
-    <?php include_once './theme-parts/footer-scripts.php'; ?>
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/resume.min.js"></script>
 
   </body>
 
